d2d7386a839d35ce795edbfba510764636a12bcb
richardrapando <richardrapando@gmail.com>
2024-01-16 09:25:55 +0300

    API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  function that queries the Reddit API """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function quering the Reddit API and returns the number of subscribers
+    queries the reddit api and returns the total number of subscribers for
+    a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux: 0x16.api.advanced: v1.0.0\
+            (by u/Charming-Zebra4864)"
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
